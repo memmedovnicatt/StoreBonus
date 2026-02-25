caff86d7cc19fbb1799cab58b8794ccec9f1eeec
memmedovnicatt <nicat.mammadov2022@khazar.org>
2026-02-25 17:16:01 +0400

    added services, impl packages and create company service and company service impl

---
+++ src/main/java/com/nicat/storebonus/services/impl/CompanyServiceImpl.java
@@ -0,0 +1,42 @@
+package com.nicat.storebonus.services.impl;
+
+import com.nicat.storebonus.dtos.request.CompanyRequest;
+import com.nicat.storebonus.dtos.response.ApiResponse;
+import com.nicat.storebonus.dtos.response.ResponseMessage;
+import com.nicat.storebonus.entities.Company;
+import com.nicat.storebonus.repositories.CompanyRepository;
+import com.nicat.storebonus.services.CompanyService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class CompanyServiceImpl implements CompanyService {
+    CompanyRepository companyRepository;
+
+
+    @Override
+    public void create(CompanyRequest companyRequest) {
+        Company company = Company.builder()
+                .location(companyRequest.location())
+                .name(companyRequest.name())
+                .build();
+
+        companyRepository.save(company);
+
+        ApiResponse.<Void>builder()
+                .data(null)
+                .message(ResponseMessage.SUCCESS_CREATE.getMessage())
+                .success(true)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+}
