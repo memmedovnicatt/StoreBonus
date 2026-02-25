caf1b42d9505ce4c86afbe72d663ff1f423e2d1b
memmedovnicatt <nicat.mammadov2022@khazar.org>
2026-02-25 12:10:15 +0400

    chore: add initial Gradle project structure

---
+++ src/main/java/com/nicat/storebonus/StoreBonusApplication.java
@@ -0,0 +1,13 @@
+package com.nicat.storebonus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBonusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreBonusApplication.class, args);
+    }
+
+}
