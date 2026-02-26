ef04e9802c054bb18fd1a1894ddb47f98181b03b
memmedovnicatt <nicat.mammadov2022@khazar.org>
2026-02-26 14:41:51 +0400

    add MarketGradeHistory entity

---
+++ src/main/java/com/nicat/storebonus/entities/MarketGradeHistory.java
@@ -0,0 +1,24 @@
+package com.nicat.storebonus.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "employers")
+public class MarketGradeHistory extends BaseEntity {
+
+    LocalDate startDate;
+    LocalDate endDate;
+    Long gradeId;
+}
