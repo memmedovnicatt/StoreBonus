1f0e2e548158f8bb4fd5652e0c8cb37ff98b9ee5
memmedovnicatt <nicat.mammadov2022@khazar.org>
2026-03-13 10:44:27 +0400

    GradeRule controller was deleted and merge to GradeController

--- src/main/java/com/nicat/storebonus/controllers/GradeController.java
+++ src/main/java/com/nicat/storebonus/controllers/GradeController.java
@@ -2,8 +2,10 @@ package com.nicat.storebonus.controllers;
 
 import com.nicat.storebonus.dtos.request.GradeCalculationRequest;
 import com.nicat.storebonus.dtos.request.GradeRequest;
+import com.nicat.storebonus.dtos.request.GradeRuleRequest;
 import com.nicat.storebonus.dtos.response.ApiResponse;
 import com.nicat.storebonus.dtos.response.ResponseMessage;
+import com.nicat.storebonus.services.GradeRuleService;
 import com.nicat.storebonus.services.GradeService;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class GradeController {
 
     GradeService gradeService;
+    GradeRuleService gradeRuleService;
 
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> createGrade(@Valid @RequestBody GradeRequest gradeRequest) {
@@ -30,5 +33,11 @@ public class GradeController {
     public ResponseEntity<ApiResponse<Void>> calculateGrade(@Valid @RequestBody GradeCalculationRequest gradeCalculationRequest) {
         gradeService.calculateGrade(gradeCalculationRequest);
         return ResponseEntity.ok(ApiResponse.success(null, ResponseMessage.SUCCESS_CALCULATED));
+    }
+
+    @PostMapping("/rules")
+    public ResponseEntity<ApiResponse<Void>> createGradePositionBonus(@Valid @RequestBody GradeRuleRequest gradeRuleRequest) {
+        gradeRuleService.create(gradeRuleRequest);
+        return ResponseEntity.ok(ApiResponse.success(null, ResponseMessage.SUCCESS_CREATE));
     }
 }
